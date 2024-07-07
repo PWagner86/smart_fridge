b7546272c7deb9e7fb7bac40d0ffbd9992612e23
Peter Wagner <peter.wagner1986@gmail.com>
2024-07-07 14:50:17 +0200

    (feature): added startscreen

--- src/ts/classes/manager.ts
+++ src/ts/classes/manager.ts
@@ -1,3 +1,4 @@
+import Startscreen from "./startScreen";
 import Clock from "./clock";
 import Controls from "./controls";
 import Weather from "./weather";
@@ -5,18 +6,21 @@ import Fridge from "./fridge";
 
 export default class Manager {
 
+    private startBtn: HTMLButtonElement;
+    private drop: HTMLDivElement;
+    private startScreen: Startscreen;
     private clock: Clock;
     private controls : Controls;
     private weather: Weather;
     private fridge: Fridge;
 
     constructor(
-
         private clockText: HTMLHeadingElement,
         private dateText: HTMLHeadingElement,
-
         ) {
-
+        this.startBtn = document.querySelector('[data-start-button]') as HTMLButtonElement;
+        this.drop = document.querySelector('[data-start-drop]') as HTMLDivElement;
+        this.startScreen = new Startscreen();
         this.clock = new Clock();
         this.controls = new Controls();
         this.weather = new Weather();
@@ -24,6 +28,7 @@ export default class Manager {
     }
 
     public init() {
+        this.startBtn.addEventListener('click', () => this.startScreen.startProgramme(this.startBtn, this.drop));
         console.log('SmartFridge started...');
         this.clock.showDate(this.clockText, this.dateText);
         this.weather.setWeatherIcon();
---
+++ src/ts/classes/startScreen.ts
@@ -0,0 +1,47 @@
+export default class Startscreen {
+
+    private startScreen: HTMLDivElement;
+    private startSound: HTMLAudioElement;
+    private letters: Array<HTMLSpanElement>;
+
+    constructor() {
+        this.startScreen = document.querySelector('[data-start-screen]') as HTMLDivElement;
+        this.startSound = document.querySelector('[data-start-up-sound]') as HTMLAudioElement;
+        this.letters = [...document.querySelectorAll('[data-start-letter]')] as Array<HTMLSpanElement>;
+        console.log(this.letters);
+    }
+    
+    public startProgramme( btn: HTMLButtonElement, drop: HTMLDivElement) {
+        btn.classList.add('start-vanish');
+        drop.classList.add('start-vanish');
+        this.startSound.play();
+        this.showHideLetters();
+        this.vanishScreen();
+        this.displayNone();
+    }
+
+    private displayNone() {
+        setTimeout(() => {
+            this.startScreen.classList.add('start-none');
+        }, 7500);
+    }
+
+    private showHideLetters() {
+        const order: Array<number> = [0, 5, 2, 10, 6, 1, 3, 9, 4, 7, 8];
+        let interval: number = 200;
+        for(let i = 0; i < order.length; i++) {
+            const index: number = order[i];
+            setTimeout(() => {
+                this.letters[index].classList.add('show');
+            }, interval)
+            interval += 350;
+        }
+        console.log(interval);
+    }
+
+    private vanishScreen() {
+        setTimeout(() => {
+            this.startScreen.classList.add('start-vanish');
+        }, 7000);
+    }
+}
